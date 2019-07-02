1d8f3ba291432126ef51728e230818cd47ef127d
yinghui130 <yinghui130@qq.com>
2019-07-02 16:40:33 +0800

    create table sql script

---
+++ create_t_sql.sql
@@ -0,0 +1,48 @@
+CREATE TABLE STUDENT (
+	PERSON_ID varchar(50) NOT NULL,
+	NAME varchar(50) NULL,
+	ACCESS_TYPE varchar(16) NULL,
+	EXAM_NO varchar(36) NULL,
+	STUDENT_NO varchar(36) NULL,
+	COLLEGE_CODE varchar(36) NULL,
+	MAJOR_CODE varchar(36) NULL,
+	MAJOR_NAME varchar(72) NULL,
+	EDU_TYPE varchar(36) NULL,
+	ADDRESS varchar(512) NULL,
+	POST_CODE varchar(8) NULL,
+	TEL_NO1 varchar(18) NULL,
+	TEL_NO2 varchar(18) NULL,
+	EMS varchar(512) NULL,
+	UPDATE_TIME datetime NULL,
+	COLLEGE_NAME varchar(128) NULL,
+	CONSTRAINT PK_STUDENT PRIMARY KEY (PERSON_ID)
+) GO
+
+
+CREATE TABLE EXAM_RESULT (
+	BMH nvarchar(36) NOT NULL,
+	BKXXFSMC nvarchar(36) NULL,
+	BKXXFS nvarchar(36) NULL,
+	BKYXSM nvarchar(36) NULL,
+	BKYXSMC nvarchar(128) NULL,
+	BKZYDM nvarchar(36) NULL,
+	BKZYMC nvarchar(128) NULL,
+	KSBH nvarchar(128) NULL,
+	XM nvarchar(128) NULL,
+	ZJHM nvarchar(128) NULL,
+	ZZLLM nvarchar(128) NULL,
+	ZZLLMC nvarchar(128) NULL,
+	ZZLL int NULL,
+	WGYM nvarchar(128) NULL,
+	WGYMC nvarchar(128) NULL,
+	WGY int NULL,
+	YWK1M nvarchar(128) NULL,
+	YWK1MC nvarchar(128) NULL,
+	YWK1 int NULL,
+	YWK2M nvarchar(128) NULL,
+	YWK2MC nvarchar(128) NULL,
+	YWK2 int NULL,
+	ZF int NULL,
+	CONSTRAINT PK_EXAM_RESULT PRIMARY KEY (BMH)
+) GO;
+
