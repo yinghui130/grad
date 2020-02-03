fcfe1993f9fb71fbbf6258e11a72b06e3d580983
yinghui130 <yinghui130@qq.com>
2020-02-03 12:52:36 +0800

    new

--- src/main/java/cn/edu/ldu/grad/controller/StudentController.java
+++ src/main/java/cn/edu/ldu/grad/controller/StudentController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import cn.edu.ldu.grad.mapper.StudentInfoMapper;
 import cn.edu.ldu.grad.pojo.StudentInfo;
@@ -27,10 +26,11 @@ public class StudentController {
     StudentInfo studentInfo = studentInfoMapper.selectByPrimaryKey(password);
     return new ResponseEntity<>(studentInfo, HttpStatus.OK);
   }
+
   @PostMapping("/update")
-  public HttpEntity<?> update(@RequestBody StudentInfo info){
+  public HttpEntity<?> update(@RequestBody StudentInfo info) {
     studentInfoMapper.updateByPrimaryKey(info);
-    return new ResponseEntity<>(true,HttpStatus.OK);
-    
+    return new ResponseEntity<>(true, HttpStatus.OK);
+
   }
 }
