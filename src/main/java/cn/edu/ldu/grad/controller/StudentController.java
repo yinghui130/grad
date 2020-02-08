54875d9c4e112f6d2516e6f5ad1bd87c1ff523b0
yinghui130 <yinghui130@qq.com>
2020-02-08 15:10:30 +0800

    finished

--- src/main/java/cn/edu/ldu/grad/controller/StudentController.java
+++ src/main/java/cn/edu/ldu/grad/controller/StudentController.java
@@ -1,5 +1,8 @@
 package cn.edu.ldu.grad.controller;
 
+import java.util.Date;
+import java.util.List;
+import org.apache.logging.log4j.util.Strings;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
@@ -9,10 +12,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import cn.edu.ldu.grad.mapper.StuExamCheckInfoMapper;
 import cn.edu.ldu.grad.mapper.StudentInfoMapper;
+import cn.edu.ldu.grad.mapper.StudentSubjectInfoMapper;
+import cn.edu.ldu.grad.pojo.StuExamCheckInfo;
 import cn.edu.ldu.grad.pojo.StudentInfo;
+import cn.edu.ldu.grad.pojo.StudentSubjectInfo;
 
 @RestController
 @CrossOrigin
@@ -20,17 +26,49 @@ import cn.edu.ldu.grad.pojo.StudentInfo;
 public class StudentController {
   @Autowired
   private StudentInfoMapper studentInfoMapper;
+  @Autowired
+  private StudentSubjectInfoMapper studentSubjectInfoMapper;
+  @Autowired
+  private StuExamCheckInfoMapper stuExamCheckInfoMapper;
 
-  @PostMapping("/login/{username}/{password}")
+  @PostMapping("/login/{username}/{password}/{type}")
   public HttpEntity<?> logIn(@PathVariable(value = "username", required = true) String username,
-      @PathVariable(value = "password", required = true) String password) {
-    StudentInfo studentInfo = studentInfoMapper.selectByPrimaryKey(password);
-    return new ResponseEntity<>(studentInfo, HttpStatus.OK);
+      @PathVariable(value = "password", required = true) String password,
+      @PathVariable(value = "type", required = false) String type) {
+    if (Strings.isBlank(type)) {
+      StudentInfo studentInfo = studentInfoMapper.selectByPrimaryKey(password);
+      return new ResponseEntity<>(studentInfo, HttpStatus.OK);
+    } else {
+      StudentSubjectInfo stuSubInfo = studentSubjectInfoMapper.selectByPrimaryKey(username);
+      return new ResponseEntity<>(stuSubInfo, HttpStatus.OK);
+    }
   }
+
   @PostMapping("/update")
-  public HttpEntity<?> update(@RequestBody StudentInfo info){
+  public HttpEntity<?> update(@RequestBody StudentInfo info) {
     studentInfoMapper.updateByPrimaryKey(info);
-    return new ResponseEntity<>(true,HttpStatus.OK);
-    
+    return new ResponseEntity<>(true, HttpStatus.OK);
+
+  }
+
+  @PostMapping("/getStuExamCheckList/{ksbh}")
+  public HttpEntity<?> getStuExamCheckList(
+      @PathVariable(value = "ksbh", required = true) String ksbh) {
+    List<StuExamCheckInfo> list = stuExamCheckInfoMapper.selectByKsbh(ksbh);
+    return new ResponseEntity<>(list, HttpStatus.OK);
+  }
+
+  @PostMapping("/saveStuExamCheckList")
+  public HttpEntity<?> saveStuExamCheckList(@RequestBody List<StuExamCheckInfo> list) {
+    if (list.size() > 0) {
+      String ksbhString = list.get(0).getKsbh();
+      stuExamCheckInfoMapper.deleteByKsbh(ksbhString);
+    }
+    Date date = new Date();
+    for (StuExamCheckInfo stuExamCheckInfo : list) {
+      stuExamCheckInfo.setSumitTime(date);
+      stuExamCheckInfoMapper.insert(stuExamCheckInfo);
+    }
+    return new ResponseEntity<>(true, HttpStatus.OK);
   }
 }
--- src/main/java/cn/edu/ldu/grad/mapper/StudentInfoMapper.java
+++ src/main/java/cn/edu/ldu/grad/mapper/StudentInfoMapper.java
@@ -1,17 +1,19 @@
 package cn.edu.ldu.grad.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
 import cn.edu.ldu.grad.pojo.StudentInfo;
 
+@Mapper
 public interface StudentInfoMapper {
-    int deleteByPrimaryKey(String personId);
+  int deleteByPrimaryKey(String personId);
 
-    int insert(StudentInfo record);
+  int insert(StudentInfo record);
 
-    int insertSelective(StudentInfo record);
+  int insertSelective(StudentInfo record);
 
-    StudentInfo selectByPrimaryKey(String personId);
+  StudentInfo selectByPrimaryKey(String personId);
 
-    int updateByPrimaryKeySelective(StudentInfo record);
+  int updateByPrimaryKeySelective(StudentInfo record);
 
-    int updateByPrimaryKey(StudentInfo record);
+  int updateByPrimaryKey(StudentInfo record);
 }
